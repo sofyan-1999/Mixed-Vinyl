cc37998352252216a77cd4306c15358ef9806e5c
Sofyan Bouazzati <sofyan.bouazzati@hva.nl>
2023-02-27 17:11:34 +0100

    followed symfony casts tutorial till part 3 till chapter 18

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,19 +2,16 @@
 
 namespace App\Controller;
 
-use App\Service\MixRepository;
-use Psr\Cache\CacheItemInterface;
+use App\Repository\VinylMixRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
     public function __construct(
-        private MixRepository $mixRepository
+        private VinylMixRepository $mixRepository
     )
     {
     }
@@ -38,10 +35,10 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $this->mixRepository->findAll();
+        $mixes = $mixRepository->findAllOrderedByVotes($slug);
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
